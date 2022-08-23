3b9a4c4ecbc2b62fe01a423008142c2850834826
MECVxD <mauricioecovarrubias@hotmail.com>
2022-08-23 13:39:15 -0500

    Saved last changes

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,16 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
+  public enMayusculas: boolean = true;
+  public ordenarPor: string = '';
+  public heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul,
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro,
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde,
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo,
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde,
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public changeValue(): boolean {
+    if (this.enMayusculas) {
+      return (this.enMayusculas = false);
+    }
+    return (this.enMayusculas = true);
+    // this.enMayusculas = !this.enMayusculas;
   }
 
+  public changeOrder(valor: string): void {
+    this.ordenarPor = valor;
+  }
 }
--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -8,12 +8,21 @@ import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { NumerosComponent } from './pages/numeros/numeros.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
+import { MayusculasPipe } from './pipes/mayusculas.pipe';
+import { OrdenarPipe } from './pipes/ordenar.pipe';
+import { VuelaPipe } from './pipes/vuela.pipe';
+
 @NgModule({
   declarations: [
+    // Componentes
     BasicosComponent,
     NoComunesComponent,
     NumerosComponent,
     OrdenarComponent,
+    // Pipes
+    MayusculasPipe,
+    OrdenarPipe,
+    VuelaPipe,
   ],
   imports: [CommonModule, PrimeNgModule],
   exports: [
